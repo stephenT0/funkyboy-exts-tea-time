0e7d017222fbd234699aea03ba41c8ace9c1eb1b
Stephen Tong/Funky Boy <stephen.tong@mail.utoronto.ca>
2022-08-24 13:30:16 -0400

    added Window reall

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Spawns a Utah Teapot at world center. Use for testing different rendering engines.
 -Extension could be eventually updated into a Cornell Box generator
--recall function from Window menu still needs to be implemented 
+
 
 ## Adding This Extension
 
--- exts/funkyboy.tea.time/funkyboy/tea/time/extension.py
+++ exts/funkyboy.tea.time/funkyboy/tea/time/extension.py
@@ -1,23 +1,69 @@
+#__all__ = ["ExampleWindowExtension"]
+
+from .window import TeaWindow
+from functools import partial
+import asyncio
 import omni.ext
+import omni.kit.ui
 import omni.ui as ui
-from .tea_pot_gen import TeapotSpawn
 
-class MyExtension(omni.ext.IExt):
 
-    def on_startup(self, ext_id):
-        print("[funkyboy.tea.test] MyExtension startup")
+class TeaTimeExtension(omni.ext.IExt):
+    """The entry point for the Tea Time Window"""
 
-        self._window = ui.Window("Tea Time", width=250, height=250)
-        with self._window.frame:
-            with ui.VStack():
-                style = {"margin_width": 20}
-                style={"background_color": 0x33FFF86}
-                ui.Button("Is it Tea Time?", style=style,)
+    WINDOW_NAME = "Tea Time"
+    MENU_PATH = f"Window/{WINDOW_NAME}"
 
-                def on_click():
-                    TeapotSpawn()
+    def on_startup(self):
+        # The ability to show up the window if the system requires it. We use it
+        # in QuickLayout.
+        ui.Workspace.set_show_window_fn(TeaTimeExtension.WINDOW_NAME, partial(self.show_window, None))
 
-                ui.Button("Make Tea", clicked_fn=lambda: on_click(), style={"Button": {"background_color":  0xFF00B976}}, tooltip="  spawns teapot at world center  ")
+        # Put the new menu
+        editor_menu = omni.kit.ui.get_editor_menu()
+        if editor_menu:
+            self._menu = editor_menu.add_item(
+                TeaTimeExtension.MENU_PATH, self.show_window, toggle=True, value=True
+            )
+
+        # Show the window. It will call `self.show_window`
+        ui.Workspace.show_window(TeaTimeExtension.WINDOW_NAME)
 
     def on_shutdown(self):
-        print("[funkyboy.tea.test] MyExtension shutdown")
+        self._menu = None
+        if self._window:
+            self._window.destroy()
+            self._window = None
+
+        # Deregister the function that shows the window from omni.ui
+        ui.Workspace.set_show_window_fn(TeaTimeExtension.WINDOW_NAME, None)
+
+    def _set_menu(self, value):
+        """Set the menu to create this window on and off"""
+        editor_menu = omni.kit.ui.get_editor_menu()
+        if editor_menu:
+            editor_menu.set_value(TeaTimeExtension.MENU_PATH, value)
+
+    async def _destroy_window_async(self):
+        # wait one frame, this is due to the one frame defer
+        # in Window::_moveToMainOSWindow()
+        await omni.kit.app.get_app().next_update_async()
+        if self._window:
+            self._window.destroy()
+            self._window = None
+
+    def _visiblity_changed_fn(self, visible):
+        # Called when the user pressed "X"
+        self._set_menu(visible)
+        if not visible:
+            # Destroy the window, since we are creating new window
+            # in show_window
+            asyncio.ensure_future(self._destroy_window_async())
+
+    def show_window(self, menu, value):
+        if value:
+            self._window = TeaWindow(TeaTimeExtension.WINDOW_NAME, width=250, height=250)
+            self._window.set_visibility_changed_fn(self._visiblity_changed_fn)
+        elif self._window:
+            self._window.visible = False
+
